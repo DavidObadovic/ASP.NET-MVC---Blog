6cb885fc7f8b9a3f7576efa30a7c673b54153b6e
DavidObadovic <davidda>
2024-11-09 00:47:29 +0100

    Add project files.

---
+++ MojProjekatPonovo/Data/BlogDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MojProjekatPonovo.Models.Domain;
+
+namespace MojProjekatPonovo.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
---
+++ MojProjekatPonovo/Models/Domain/BlogPost.cs
@@ -0,0 +1,18 @@
+﻿namespace MojProjekatPonovo.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+        public string Heading { get; set; }
+        public string PageTitle { get; set; }
+        public string Contect { get; set; }
+        public string ShortDecription { get; set; }
+        public string FeaturedImageUrl { get; set; }
+        public string UrlHandle { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Author { get; set; }
+        public bool Visible { get; set; }
+        public ICollection<Tag> Tags { get; set; }
+
+    }
+}
