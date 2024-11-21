fd44e2317803dc30b0c8560423df973f6c20c8c2
DavidObadovic <davidda>
2024-11-21 13:23:19 +0100

    Asihrone metode

--- MojProjekatPonovo/Controllers/AdminTagsController.cs
+++ MojProjekatPonovo/Controllers/AdminTagsController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
 using MojProjekatPonovo.Data;
 using MojProjekatPonovo.Models.Domain;
 using MojProjekatPonovo.Models.ViewModels;
@@ -20,27 +22,82 @@ namespace MojProjekatPonovo.Controllers
         }
 
         [HttpPost]
-        public IActionResult Add(AddTagRequest addTagRequest)
+        public async Task<IActionResult> Add(AddTagRequest addTagRequest)
         {
             var tag = new Tag
             {
 
-                Name= addTagRequest.Name,
-                DisplayName= addTagRequest.DisplayName,
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName,
             };
 
-            _blogDbContext.Tags.Add(tag);
-            _blogDbContext.SaveChanges();
-            return RedirectToAction("List"); 
+            await _blogDbContext.Tags.AddAsync(tag);
+            await _blogDbContext.SaveChangesAsync();
+            return RedirectToAction("List");
         }
 
         [HttpGet]
         [ActionName("List")]
-        public IActionResult List()
+        public async Task<IActionResult> List()
         {
-            var tags = _blogDbContext.Tags.ToList();
+            var tags = await _blogDbContext.Tags.ToListAsync();
 
             return View(tags);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            //1st method
+            //var tag = _blogDbContext.Tags.Find(id);
+
+            var tag = await _blogDbContext.Tags.FirstOrDefaultAsync(x => x.Id == id);
+            if (tag != null)
+            {
+                var editTag = new EditTagRequest();
+                editTag.Name = tag.Name;
+                editTag.DisplayName = tag.DisplayName;
+                editTag.Id = tag.Id;
+                return View(editTag);
+            }
+
+            return View(null);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditTagRequest editTagRequest)
+        {
+            var tag = new Tag()
+            {
+                Id = editTagRequest.Id,
+                Name = editTagRequest.Name,
+                DisplayName = editTagRequest.DisplayName,
+            };
+            var existTag = await _blogDbContext.Tags.FindAsync(tag.Id);
+            if (existTag != null)
+            {
+                existTag.Name = tag.Name;
+                existTag.DisplayName = tag.DisplayName;
+                await _blogDbContext.SaveChangesAsync();
+                return RedirectToAction("List");
+            }
+
+
+            return RedirectToAction("Edit", new { id = editTagRequest.Id });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(EditTagRequest editTagRequest)
+        {
+            var tag = await _blogDbContext.Tags.FindAsync(editTagRequest.Id);
+            if (tag != null)
+            {
+                _blogDbContext.Remove(tag);
+                await _blogDbContext.SaveChangesAsync();
+                //show succes
+                return RedirectToAction("List");
+            }
+            return RedirectToAction("List");
         }
     }
 }
